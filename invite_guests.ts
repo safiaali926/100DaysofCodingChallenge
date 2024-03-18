044c62edc20472e368c57d210d9d8e758432f0e4
safiaali926 <163167812+safiaali926@users.noreply.github.com>
2024-03-18 22:37:11 +0500

    Delete invite_guests.ts

--- invite_guests.ts
+++
@@ -1,4 +0,0 @@
-let guestslist:string[] = ['Tasmia', 'Huzaifa', 'Madiha']
-guestslist.forEach (guests => {
-    console.log(`Hi ${guests}, Hope you're doing awesome! Join me for my birthday bash on May 9th and bring along a gift to make it even more special!`)
-})
