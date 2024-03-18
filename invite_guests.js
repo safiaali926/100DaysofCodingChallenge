4d25824dfe4c134bd416b7141bac1dc9db406e03
safiaali926 <163167812+safiaali926@users.noreply.github.com>
2024-03-18 22:36:53 +0500

    Delete invite_guests.js

--- invite_guests.js
+++
@@ -1,4 +0,0 @@
-var guestslist = ['Tasmia', 'Huzaifa', 'Madiha'];
-guestslist.forEach(function (guests) {
-    console.log("Hi ".concat(guests, ", Hope you're doing awesome! Join me for my birthday bash on May 9th and bring along a gift to make it even more special!"));
-});
